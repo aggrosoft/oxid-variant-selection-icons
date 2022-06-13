ae9f3b5733ebf58dbc502e3a0997e611015758bd
alexander <coding@aggrosoft.de>
2022-06-13 10:37:52 +0200

    added getter for variants color code

--- copy_this/modules/agvarianticons/extensions/agvarianticons_oxarticle.php
+++ copy_this/modules/agvarianticons/extensions/agvarianticons_oxarticle.php
@@ -7,4 +7,10 @@ class agvarianticons_oxarticle extends agvarianticons_oxarticle_parent {
         return false; //Load full variants in list so we can show icons
     }
 
+    public function getSelectionHexColor () {
+        $config = oxRegistry::getConfig();
+        $map = $config->getShopConfVar('aColorMap', null, 'module:agvarianticons');
+        return $map[$this->oxarticles__oxvarselect->value];
+    }
+
 }
